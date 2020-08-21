6b06374ed1e3e8208d729542f0057b31d4d8cfbb
hulipeng@zhibitech.com <12345678>
2020-08-21 18:19:21 +0800

    Initial commit

---
+++ src/views/system/index/page.vue
@@ -0,0 +1,28 @@
+<template>
+  <div style="margin-top: 60px;margin-left: 600px;width: 700px;">
+    <el-input placeholder="请输入内容" v-model="input3" class="input-with-select" clearable=true>
+      <el-select v-model="select" slot="prepend" placeholder="请选择">
+        <el-option label="餐厅名" value="1"></el-option>
+        <el-option label="订单号" value="2"></el-option>
+        <el-option label="用户电话" value="3"></el-option>
+      </el-select>
+      <el-button slot="append" icon="el-icon-search"></el-button>
+    </el-input>
+  </div>
+</template>
+<script>
+export default {
+  name:"",
+  data(){
+    return {
+      input3: '',
+      select: ''
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
